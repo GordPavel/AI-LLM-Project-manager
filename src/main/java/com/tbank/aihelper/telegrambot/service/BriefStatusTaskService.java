679fce979f0ac22c498c0c8d52d8cf3617f585f7
jxkrbdy <jakuresi@mail.ru>
2025-11-27 17:48:16 +0400

    Added question context

--- src/main/java/com/tbank/aihelper/telegrambot/service/BriefStatusTaskService.java
+++ src/main/java/com/tbank/aihelper/telegrambot/service/BriefStatusTaskService.java
@@ -1,11 +1,14 @@
 package com.tbank.aihelper.telegrambot.service;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 import com.tbank.aihelper.llm.LLMAdapter;
 import com.tbank.aihelper.telegrambot.ChatBotAdapter;
 import com.tbank.aihelper.telegrambot.dto.BotMessage;
 import com.tbank.aihelper.telegrambot.dto.UpdateContext;
+import com.tbank.aihelper.telegrambot.entity.ChatConfiguration;
 import com.tbank.aihelper.telegrambot.observer.EventListenerChatBot;
 import com.tbank.aihelper.telegrambot.observer.ObserverChatBotAdapter;
 import com.tbank.aihelper.telegrambot.repository.ChatConfigurationRepository;
@@ -33,12 +36,28 @@ public class BriefStatusTaskService implements EventListenerChatBot {
 
     public void handleNewMessage(UpdateContext updateContext) {
         
-        if(!updateContext.getCommand().equals(HANDLE_COMMAND))
+        if(updateContext.getCommand() == null 
+            || !updateContext.getCommand().equals(HANDLE_COMMAND))
+            return;
+
+        if(updateContext.getTextMessage() == null 
+            || updateContext.getTextMessage().isEmpty())
             return;
         
-        // chatConfigurationRepository.findByChatId(updateContext.getChatId());
-        
-        String llmAnswer = llmAdapter.ask(updateContext.getTextMessage());
+        Optional<ChatConfiguration> oAnswer = chatConfigurationRepository.findByChatId(updateContext.getChatId());
+        if(oAnswer.isEmpty()) {
+            chatBotAdapter.sendMessage(BotMessage.builder()
+                .chatId(updateContext.getChatId())
+                .replyToMessageId(updateContext.getMessageId())
+                .textMessage("Конфигурация бота не найдена.")
+            .build());
+            return;
+        }
+
+        String llmAnswer = llmAdapter.ask(
+            "Контекст: " + oAnswer.get().getConfiguration() +
+            "Вопрос: " + updateContext.getTextMessage()
+        );
         
         chatBotAdapter.sendMessage(BotMessage.builder()
                 .chatId(updateContext.getChatId())
